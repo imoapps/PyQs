d4220435f6e70e2cbb22262ce3723bbc8f50617b
imoapps <passacode@gmail.com>
2014-11-21 17:54:11 -0800

    code segment completion
    increment factor included, 1 tile added as estras for every 75 square
    feet of area

--- floor_estimator.py
+++ floor_estimator.py
@@ -9,36 +9,38 @@ def area ():
     in decimal inches :\n")
     tile_width = raw_input("Enter the width of the selected tile,\n \
     in decimal inches :\n")
+    
      # calculate data 
     floor_area = float(length) * float(width)
-    tile_area = float(tile_length)* float(tile_width)
-
+    tile_area = (float(tile_length)* float(tile_width))/144
+    #get the amount of tile needed , raw data
     tile_amount = floor_area/tile_area
-    
+
+    # add 1 tile per 75 squarefeet  of area
+    extra = floor_area/75
+    extra=round(extra)
+    tile_amount = tile_amount+extra
+       
     # return raw data
     return (length,width,floor_area,tile_amount)
 
 # generate a floor report
 
-def floorReport ():
-    
-    e,w,a,ta=area() # call to area function 
-    e=round(e,2) # tidying up  the data 
-    w=round(w,2)
-    a=round(a,2)
-    ta=round(ta)
-    # add 2 extra tiles per 100 sqft of space
-    test = a/100.
-    print 'test {}'.format(test)
-    #if a/100>1:
-       # ta=int(ta)+2  # add t
-    
-    
-    print "Length of area : {} feet.\n\
+def floorReport ():    
+    length,width,flr_area,tileAmount=area() # call to area function 
+     # tidying up  the data
+    flr_area=round(flr_area,2)
+    tileAmount = int(round(tileAmount))
+
+    #final output    
+    print "\n\t Length of area : {} feet.\n\
                 Width of area : {} feet.\n \
-                Area calculated : {} square meters \n\
-                Amount of Tiles required for the job : {} each ".format(e,w,a,ta)
+                Area calculated : {} square feet \n\
+                Amount of Tiles required for the job : {} each ".format(length,
+                                                                                            width,flr_area,tileAmount)
 
 floorReport()
 
+##............................>>>>>>> END
+## MORE TO COME
 
